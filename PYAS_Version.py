2b75dc14804ba36b5d2b68ffe93f36bf103435a9
jeb1399 <145628605+jeb1399@users.noreply.github.com>
2024-04-07 23:25:48 -0400

    Update PYAS_Version.py

--- PYAS_Version.py
+++ PYAS_Version.py
@@ -1,11 +1,12 @@
 import pyinstaller_versionfile
 
 pyinstaller_versionfile.create_versionfile(
-output_file="versionfile.txt",
-version='3.0.9',
-company_name="PYAS Security",
-file_description="Python Antivirus Software",
-internal_name="PYAS",
-legal_copyright="PYAS Security",
-original_filename="PYAS.exe",
-product_name="PYAS")
+    output_file="versionfile.txt",
+    version='3.0.9',
+    company_name="PYAS Security",
+    file_description="Python Antivirus Software",
+    internal_name="PYAS",
+    legal_copyright="PYAS Security",
+    original_filename="PYAS",
+    product_name="PYAS"
+)
